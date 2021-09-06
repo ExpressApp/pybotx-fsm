e4fe12522a2ea8b390d21e5c06081f4ebb3cad27
vkarabanov <vkarabanov@ccsteam.ru>
2021-09-06 13:50:06 +0300

    fix: tests for Python 3.7

--- botx_fsm/markers.py
+++ botx_fsm/markers.py
@@ -1,4 +1,11 @@
-from typing import Final, final
+try:
+    from typing import Final, final  # noqa: WPS433
+except ImportError:
+    # Python 3.7 don't have Final type
+    from typing import TypeVar  # noqa: WPS433
+
+    Final = TypeVar("Final")  # noqa: WPS440
+    final = lambda x: x  # noqa: E731,WPS440,WPS111
 
 
 @final
