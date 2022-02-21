87a3c53d7a562dcabeba76703e0446218eec65b5
Yegor Kutuzov <kutuzov-1313@yandex.ru>
2022-02-21 15:58:39 +0300

    Add test changing state on successful.

--- tests/test_fsm/test_change_state.py
+++ tests/test_fsm/test_change_state.py
@@ -1,4 +1,7 @@
+import asyncio
 from enum import Enum, auto
+from typing import Callable
+from unittest.mock import AsyncMock
 
 import pytest
 from botx import (
@@ -11,39 +14,82 @@ from botx import (
 from pytest import mark
 
 from botx_fsm import FSMCollector, FSMMiddleware
+from tests.state_repo import StateRepo
 
 
 class EnumForTests(Enum):
-    STATE1 = auto()
-    STATE2 = auto()
+    FIRST_STATE = auto()
+    SECOND_STATE = auto()
 
 
 @pytest.fixture
-def fsm() -> FSMCollector:
+def first_handler() -> AsyncMock:
+    async def process_first_state(message: IncomingMessage, bot: Bot) -> None:
+        await message.state.fsm.change_state(EnumForTests.SECOND_STATE)
+
+    return AsyncMock(wraps=process_first_state)
+
+
+@pytest.fixture
+def second_handler() -> AsyncMock:
+    async def process_second_state(message: IncomingMessage, bot: Bot) -> None:
+        await message.state.fsm.drop_state()
+
+    return AsyncMock(wraps=process_second_state)
+
+
+@pytest.fixture
+def fsm(first_handler: AsyncMock, second_handler: AsyncMock) -> FSMCollector:
     fsm = FSMCollector(EnumForTests)
 
-    @fsm.on(EnumForTests.STATE1)
-    async def process_state1(message: IncomingMessage) -> None:
-        await message.state.fsm.change_state(message, EnumForTests.STATE2)
-
-    @fsm.on(EnumForTests.STATE2)
-    async def process_state2(message: IncomingMessage) -> None:
-        await message.state.fsm.drop_state(message)
+    fsm.on(EnumForTests.FIRST_STATE)(first_handler)
+    fsm.on(EnumForTests.SECOND_STATE)(second_handler)
 
     return fsm
 
 
-@pytest.fixture()
-def fsm_bot(fsm: FSMCollector, bot_account: BotAccountWithSecret) -> Bot:
-    built_bot = Bot(
-        collectors=[HandlerCollector()],
-        bot_accounts=[bot_account],
-        middlewares=[FSMMiddleware([fsm], state_repo_key="redis_repo")],
-    )
+@pytest.fixture
+def bot(fsm: FSMCollector, bot_account: BotAccountWithSecret) -> Bot:
+    collector = HandlerCollector()
 
+    @collector.command("/fsm", visible=False)
+    async def fsm_handler(message: IncomingMessage, bot: Bot) -> None:
+        await message.state.fsm.change_state(EnumForTests.FIRST_STATE)
+
+    @collector.default_message_handler
+    async def default_handler(message: IncomingMessage, bot: Bot) -> None:
+        return
+
+    built_bot = Bot(
+        collectors=[collector],
+        bot_accounts=[bot_account],
+        middlewares=[FSMMiddleware([fsm], state_repo_key="state_repo")],
+    )
+    built_bot.state.state_repo = StateRepo()
     return built_bot
 
 
 @mark.asyncio
-async def test_changing_state_on_successful(fsm_bot: Bot) -> None:
-    ...
+async def test_changing_state_on_successful(
+    bot: Bot,
+    incoming_message_factory: Callable[..., IncomingMessage],
+    first_handler: AsyncMock,
+    second_handler: AsyncMock,
+) -> None:
+    # - Arrange -
+    start_message = incoming_message_factory(body="/fsm")
+    first_message = incoming_message_factory(body="text")
+    second_message = incoming_message_factory(body="text")
+
+    # - Act -
+    async with lifespan_wrapper(bot) as bot:
+        bot.async_execute_bot_command(start_message)
+        await asyncio.sleep(0)
+        bot.async_execute_bot_command(first_message)
+        await asyncio.sleep(0)
+        bot.async_execute_bot_command(second_message)
+        await asyncio.sleep(0)
+
+    # - Assert -
+    first_handler.assert_awaited_once_with(first_message, bot)
+    second_handler.assert_awaited_once_with(second_message, bot)
