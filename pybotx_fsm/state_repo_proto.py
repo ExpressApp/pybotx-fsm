f28560018791caa8ac56ecc31badc75d7332d1b4
osovskiyalexandr <osovskiy.alexandr@ccsteam.ru>
2025-05-16 11:08:49 +0300

    chore: drop python38, add python313
     chore: drop python38, add python313

--- pybotx_fsm/state_repo_proto.py
+++ pybotx_fsm/state_repo_proto.py
@@ -4,16 +4,13 @@ from typing_extensions import Protocol
 
 
 class StateRepoProto(Protocol):
-    async def get(self, key: Hashable, default: Any = None) -> Any:
-        ...  # noqa: WPS428
+    async def get(self, key: Hashable, default: Any = None) -> Any: ...  # noqa: WPS428
 
     async def set(
         self,
         key: Hashable,
         value: Any,
         expire: Optional[int] = None,
-    ) -> None:
-        ...  # noqa: WPS428
+    ) -> None: ...  # noqa: WPS428
 
-    async def delete(self, key: Hashable) -> None:
-        ...  # noqa: WPS428
+    async def delete(self, key: Hashable) -> None: ...  # noqa: WPS428
